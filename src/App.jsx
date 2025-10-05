4aff7010d7b81326e77935bb7b69d4f76d3fddad
shahirsizan <shahir.sizan18@gmail.com>
2025-10-06 00:02:07 +0600

    App component updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,35 @@
+import { useState } from "react";
+import Header from "./components/Header";
+import HomePage from "./components/HomePage";
+import FileDisplay from "./components/FileDisplay";
+
 function App() {
-	return <div className="h-screen bg-yellow-500">Hello Sizan!</div>;
+	const [file, setFile] = useState(null);
+	const [audioStream, setAudioStream] = useState(null);
+	const isAudioAvailable = file || audioStream;
+
+	const handleAudioReset = () => {
+		setFile(null);
+		setAudioStream(null);
+	};
+
+	return (
+		<div className="flex flex-col px-[5vw] mdpx-[8vw] xl:px-[12vw]">
+			<section className="min-h-screen  flex flex-col">
+				<Header />
+
+				{isAudioAvailable ? (
+					<FileDisplay
+						handleAudioReset={handleAudioReset}
+						file={file}
+						audioStream={audioStream}
+					/>
+				) : (
+					<HomePage setFile={setFile} audioStream={audioStream} />
+				)}
+			</section>
+		</div>
+	);
 }
 
 export default App;
