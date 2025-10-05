c0bde32c684760544335b0c6c79da11c92124ad2
shahirsizan <shahir.sizan18@gmail.com>
2025-10-05 22:46:32 +0600

    Header component added

---
+++ src/components/Header.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { FaPlus } from "react-icons/fa";
+
+export default function Header() {
+	return (
+		<header className="flex items-center justify-between gap-4 py-4">
+			<a href="/">
+				<h1 className="font-medium">
+					Tran<span className="text-blue-400 font-bold">Scribe</span>
+				</h1>
+			</a>
+
+			<div className="gap-4 flex items-center ">
+				<a
+					href="https://bmc.link/smoljames"
+					target="_blank"
+					className="text-slate-600 cursor-pointer"
+					rel="noreferrer"
+				>
+					Donate
+				</a>
+
+				<a
+					href="/"
+					className="flex items-center gap-2 specialBtn px-3 py-2 rounded-lg text-blue-400"
+				>
+					<p>New</p>
+					<FaPlus />
+				</a>
+			</div>
+		</header>
+	);
+}
